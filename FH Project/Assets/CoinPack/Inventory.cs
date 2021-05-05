58038544fbba57b5d442ca47152a5384293f3bb4
Ohrlappi <80787849+Ohrlappi@users.noreply.github.com>
2021-05-05 14:42:29 +0200

    Gesamtes Projekt, Stand 05.05
    Map
    Spieler
    Türen
    Inventar
    Items
    Taschenlampe

---
+++ FH Project/Assets/CoinPack/Inventory.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public const int inventorySize = 20;
+    public string[] itemList = new string[inventorySize];
+
+    //Inventory shown on screen and title
+    void OnGUI()
+    {
+        GUI.Box(new Rect(100, 100, 150, 100), inventoryToString());
+    }
+
+    public int AddItem(string item)
+    {
+        for (int i = 0; i < itemList.Length; i++)
+        {
+            if (itemList[i] != null)
+            {
+                itemList[i] = item;
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    public string inventoryToString()
+    {
+        string itemNames = "Inventory\n";
+        for (int i = 0; i < itemList.Length; i++)
+        {
+            if (itemList[i] != null)
+            {
+                itemNames = itemNames + itemList[i] + "\n";
+            }
+
+        }
+        return itemNames;
+    }
+}
---
+++ FH Project/Assets/CoinPack/Item.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public int itemId = 0;
+    public string itemName = "item";
+    public string description = "Does New Things";
+    public enum Type { Default = 0, Consumable, Weapon }
+    public Type type = Type.Default;
+    public Inventory inv;
+
+    
+    void Start()
+    {
+        inv = FindObjectOfType<Inventory>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            var radius = 10;
+            var selected = Physics.Raycast(Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2,
+                    Camera.main.nearClipPlane)), out hit, radius);
+            if (selected) 
+            { 
+                if (hit.collider.CompareTag("collectable"))
+                {
+                    inv.AddItem(this.itemName);
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+    }
+}
