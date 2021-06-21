b7a4e092589271248ade39bf94ec5397f5a77690
Ohrlappi(NIKI) <if20b309@technikum-wien.at>
2021-06-21 15:43:29 +0200

    minotaur rotation when moving
    kein moonwalk mehr

--- FH Project/Assets/Scripts/EnemyFollowsPath.cs
+++ FH Project/Assets/Scripts/EnemyFollowsPath.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class EnemyFollowsPath : MonoBehaviour
 {
     public Transform[] target;
-    public float speed;
+    public float speed = 5f;
     private int current;
+
 
     public bool activated = false;
 
     void Update()
     {
         if(activated){
+            
 
             if(transform.position != target[current].position)
             {
                 Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
                 GetComponent<Rigidbody>().MovePosition(pos);
+
+                //rotate towards next target
+                Vector3 direction = target[current].position - transform.position;
+                Quaternion rotation = Quaternion.LookRotation(direction);
+                transform.rotation = rotation;
+                
             }        
             else
             {
