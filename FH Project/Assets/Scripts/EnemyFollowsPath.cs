100c4c9a2f686cbee1a583b8da2f2253906b9a12
Ohrlappi(NIKI) <if20b309@technikum-wien.at>
2021-06-21 22:19:12 +0200

    Minotaur Chase + Animation When Player in View of Sight

--- FH Project/Assets/Scripts/EnemyFollowsPath.cs
+++ FH Project/Assets/Scripts/EnemyFollowsPath.cs
@@ -5,32 +5,73 @@ using UnityEngine;
 public class EnemyFollowsPath : MonoBehaviour
 {
     public Transform[] target;
-    public float speed = 5f;
+    public float speed;
     private int current;
+    public Transform player;
+    public bool isChasing;
+
+    Animator anim;
+    public bool activated;
 
 
-    public bool activated = false;
+
+    private void Awake()
+    {
+        player = GameObject.Find("First Person Player").transform;
+        //agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        anim = GameObject.Find("Mesh").GetComponent<Animator>();
+
+    }
 
     void Update()
     {
-        if(activated){
-            
-
-            if(transform.position != target[current].position)
+        if (activated)
+        {
+            //Check for sight
+            Vector3 targetDirection = player.position - transform.position;
+            float angle = Vector3.Angle(targetDirection, transform.forward);
+            if (angle < 45.0f)
             {
-                Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
-                GetComponent<Rigidbody>().MovePosition(pos);
-
-                //rotate towards next target
-                Vector3 direction = target[current].position - transform.position;
-                Quaternion rotation = Quaternion.LookRotation(direction);
-                transform.rotation = rotation;
-                
-            }        
+                isChasing = true;
+                speed = 5.0f;
+                anim.SetBool("isChasing", true);
+            }
             else
             {
-                current = (current + 1) % target.Length;
+                isChasing = false;
+                speed = 1.5f;
+                anim.SetBool("isChasing", false);
             }
+            Patrolling();
+
         }
     }
+
+
+
+    private void Patrolling()
+    {
+        if (transform.position != target[current].position)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+            transform.LookAt(target[current]);
+
+
+            //rotate towards next target..
+            //Vector3 direction = target[current].position - transform.position;
+            //Quaternion rotation = Quaternion.LookRotation(direction);
+            //transform.rotation = rotation;
+        }
+        else
+        {
+            current = (current + 1) % target.Length;
+        }
+    }
+
+
 }
