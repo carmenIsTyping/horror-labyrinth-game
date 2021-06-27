3fd07e44fef4841ab501068511e59d9c119f2cdf
carmenIsTyping <carmenhejtmanek@gmail.com>
2021-06-27 18:57:42 +0200

    Schlussgeräusch

--- FH Project/Assets/EndScreen.cs
+++ FH Project/Assets/EndScreen.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class EndScreen : MonoBehaviour
 {
     public GameObject Endscrn;
+    public AudioClip Win;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             Endscrn.gameObject.SetActive(true);
+            GetComponent<AudioSource>().PlayOneShot(Win);
         }
     }
 }
