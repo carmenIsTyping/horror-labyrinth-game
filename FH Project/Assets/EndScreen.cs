8736ba678659c80e23b8293470ab73264cec1171
Ohrlappi(NIKI) <if20b309@technikum-wien.at>
2021-06-27 19:33:18 +0200

    Revert "Schlussgeräusch"
    This reverts commit 3fd07e44fef4841ab501068511e59d9c119f2cdf.

--- FH Project/Assets/EndScreen.cs
+++ FH Project/Assets/EndScreen.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class EndScreen : MonoBehaviour
 {
     public GameObject Endscrn;
-    public AudioClip Win;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             Endscrn.gameObject.SetActive(true);
-            GetComponent<AudioSource>().PlayOneShot(Win);
         }
     }
 }
